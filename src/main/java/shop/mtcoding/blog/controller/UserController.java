7495f22b23ea04402e1e7dd4e840bb880e642e58
wltkd22 <wltkd22@naver.com>
2023-02-03 14:04:39 +0900

    회원가입 기능추가

--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -1,10 +1,17 @@
 package shop.mtcoding.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.blog.model.UserRepository;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/loginForm")
     public String loginForm() {
@@ -14,6 +21,16 @@ public class UserController {
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(String username, String password, String email) {
+        int result = userRepository.insert(username, password, email);
+        if (result == 1) {
+            return "redirect:/loginForm";
+        } else {
+            return "redirect:/joinForm";
+        }
     }
 
     @GetMapping("/user/updateForm")
