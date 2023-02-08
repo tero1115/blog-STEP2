b9488c5cf620008ef39187fd03e2e96d3f358202
wltkd22 <wltkd22@naver.com>
2023-02-08 14:44:29 +0900

    Jsoup 라이브러리 빌드 및 thumbnail 필드 추가

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -9,7 +9,8 @@ create table user_tb(
 create table board_tb(
     id int auto_increment primary key,
     title varchar(100) not null,
-    content varchar not null,
+    content longtext not null,
+    thumbnail longtext not null,
     user_id int not null,
     created_at timestamp not null
 );
